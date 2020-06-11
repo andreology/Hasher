2f95f8af3b83d5c0b649e1526f66163f428d77eb
andreology27 <37252145+andreology@users.noreply.github.com>
2020-06-11 11:38:58 -0700

    adding unit check to check template home.html has Hello

--- hasher/hashing/tests.py
+++ hasher/hashing/tests.py
@@ -2,26 +2,32 @@ from django.test import TestCase
 from selenium import webdriver
 
 #functional test class
-class FunctionalTestCase(TestCase):
+# class FunctionalTestCase(TestCase):
+#
+#     def setUp(self):
+#         self.browser = webdriver.Firefox()
+#
+#     def test_homepage_isup(self):
+#         self.browser.get('http://localhost:8000')
+#         self.assertIn('Enter hash here:', self.browser.page_source)
+#
+#     def test_hash_of_hello(self):
+#         self.browser.get('http://localhost:8000')
+#         self.assertIn('Enter hash here:', self.browser.page_source)
+#         #find element on the screen where text can be input
+#         text = self.browser.find_element_by_id('id_text')
+#         text.send_keys('hello')
+#         self.browser.find_element_by_name('submit').click()
+#         #result hash should be displayed
+#         self.assertIn('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824',
+#                       self.browser.page_source)
+#
+#     def tearDown(self):
+#         #shutdown and close browser
+#         self.browser.quit()
 
-    def setUp(self):
-        self.browser = webdriver.Firefox()
+class UnitTestCase(TestCase):
 
-    def test_homepage_isup(self):
-        self.browser.get('http://localhost:8000')
-        self.assertIn('Enter hash here:', self.browser.page_source)
-
-    def test_hash_of_hello(self):
-        self.browser.get('http://localhost:8000')
-        self.assertIn('Enter hash here:', self.browser.page_source)
-        #find element on the screen where text can be input
-        text = self.browser.find_element_by_id('id_text')
-        text.send_keys('hello')
-        self.browser.find_element_by_name('submit').click()
-        #result hash should be displayed
-        self.assertIn('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824',
-                      self.browser.page_source)
-
-    def tearDown(self):
-        #shutdown and close browser
-        self.browser.quit()
+    def test_homepage_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'hashing/home.html')
