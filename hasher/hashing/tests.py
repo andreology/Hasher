5f117b0c993b4526895ddfd1ee72bf1875da8626
andreology27 <37252145+andreology@users.noreply.github.com>
2020-06-11 11:30:56 -0700

    adding functional test to check hello hash is valid on homepage

--- hasher/hashing/tests.py
+++ hasher/hashing/tests.py
@@ -9,7 +9,18 @@ class FunctionalTestCase(TestCase):
 
     def test_homepage_isup(self):
         self.browser.get('http://localhost:8000')
-        self.assertIn('install', self.browser.page_source)
+        self.assertIn('Enter hash here:', self.browser.page_source)
+
+    def test_hash_of_hello(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Enter hash here:', self.browser.page_source)
+        #find element on the screen where text can be input
+        text = self.browser.find_element_by_id('id_text')
+        text.send_keys('hello')
+        self.browser.find_element_by_name('submit').click()
+        #result hash should be displayed
+        self.assertIn('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824',
+                      self.browser.page_source)
 
     def tearDown(self):
         #shutdown and close browser
